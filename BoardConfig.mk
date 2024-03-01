17c3922bfd3fdb48110e89f6eb3ca3c04e2e4bd3
giasen <giammasena@gmail.com>
2024-03-01 22:25:51 +0100

    polaris: switch to real common kernel
    same as older

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_OTA_ASSERT_DEVICE := polaris
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/xiaomi/polaris
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_CONFIG := polaris_defconfig
 TARGET_KERNEL_CLANG_VERSION := prelude
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
